29c2c1f1d2a20220ed3514e03c6dc4feefefeb3e
Pierre Zemb <contact@pierrezemb.fr>
2017-05-14 21:47:43 +0200

    fix(publish): working now

--- publish.sh
+++ publish.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+set -e;
+
+# if [[ $(git status -s) ]]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
 
 echo "Deleting old publication"
 rm -rf public
-git clone .git --branch gh-pages public
+git clone git@github.com:PierreZ/blog.git --branch gh-pages public
 hugo
 cd public 
+
 echo "pushing..."
-git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages && cd ..
 echo "done"
